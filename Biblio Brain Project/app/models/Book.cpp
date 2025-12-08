2a1284c7c05badf964e55a4adb5e104abcf9afc1
Toka Osama <tokaosama896@gmail.com>
2025-12-08 23:31:19 +0200

    uploade book model

--- Biblio Brain Project/app/models/Book.cpp
+++ Biblio Brain Project/app/models/Book.cpp
@@ -1,116 +1,70 @@
-#pragma once
-#include "../../core/BaseModel.hpp"
-#include <string>
-using namespace std;
+#include "Book.h"
 
-struct Book {
-    int id;
-    string title;
-    string author;
-    string description;
-    string category; 
-    int numberOfPages;
-    int year;
-    int totalCopies;
-    int availableCopies;
+Book::Book()
+    : id(-1), title(""), author(""), year(0),description(""),
+availableCopies(0), category("") , imagepath(""){}
 
-    Book() : id(-1), title(""), author(""), description("") , numberOfPages(0) , year(0), totalCopies(0), availableCopies(0), category("") {}
-     static Book fromJson(const json &j) {
-
-      Book b;
-      b.id = j["id"].get<int>();
-      b.title = j["title"].get<string>();
-      b.author = j["author"].get<string>();
-      b.description = j["description"].get<string>();
-      b.category = j["category"].get<string>();
-      b.year = j["year"].get<int>(); 
-      b.totalCopies = j["total_copies"].get<int>(); 
-      b.availableCopies = j["available_copies"].get<int>(); 
-      return b;
-
+Book Book::fromJson(const json& j) {
+    Book b;
+    b.id = j["id"];
+    b.title = j["title"];
+    b.author = j["author"];
+    b.description=j["description"];
+    b.year = j["year"];
+    b.imagepath=j["image_path"];
+    b.availableCopies = j["available_copies"];
+    b.category = j["category"];
+    return b;
 }
 
-
- json toJson() const {
+json Book::toJson() const {
     return {
-        {"id", id},
-        {"title", title},
-        {"author", author},
-        {"description",description},
-        {"category", category}
-        {"year", year},
-        {"total_copies", totalCopies},
-        {"available_copies", availableCopies}
+            {"id", id},
+            {"title", title},
+            {"author", author},
+            {"year", year},
+            {"available_copies", availableCopies},
+            {"category", category},
+            {"description",description},
+            {"image_path",imagepath}
     };
- }
+}
 
- bool isValid() const { return id !=-1; }
- bool isAvailable() const { return availableCopies > 0; }
+bool Book::isValid() const { return id != -1; }
+bool Book::isAvailable() const { return availableCopies > 0; }
 
-};
+BookModel::BookModel() : BaseModel("storage/books.json") {}
 
- class BookModel : public BaseModel<Book> {
-    public:
-      BookModel() : BaseModel("storage/books.json"){}
+void BookModel::create(const Book& book) {
+    json data = getAllJson();
+    json bookJson = book.toJson();
+    bookJson["id"] = generateId();
+    data.push_back(bookJson);
+    saveJson(data);
+}
 
-
-      void create(const Book &book){
-        json data = getAllJson();
-        data.push_back(book.toJson());
-        data.back()["id"] = generateId();
-        saveJson(data);
-      }
-
-
-      vector<Book> search(const string &query){
-        vector<Book> results;
-        string lowerQuery = toLower(query);
-        for (auto &book : all()){
-            if (toLower(book.title).find(lowerQuery)) != string::npos||
-            toLower(book.author).find(lowerQuery) != string::npos ||
-             toLower(book.category).find(lowerQuery) != string::npos  {
-             results.push_back(book);
+bool BookModel::decreaseAvailability(int bookId) {
+    json data = getAllJson();
+    for (auto& item : data) {
+        if (item["id"] == bookId && item["available_copies"] > 0) {
+            item["available_copies"] = item["available_copies"].get<int>() - 1;
+            saveJson(data);
+            return true;
         }
+    }
+    return false;
+}
 
-      }
-      return results;
-      }
-
-
-      bool decreaseAvailability (int bookId){
-        json data = getAllJson();
-        for (auto &item : data){
-            if (item["id"] == bookId && item["available_copies"] > 0){
-                item["available_copies"] = item["available_copies"].get<int>()- 1;
-                saveJson(data);
-                return true;
-            }
+bool BookModel::increaseAvailability(int bookId) {
+    json data = getAllJson();
+    for (auto& item : data) {
+        if (item["id"] == bookId) {
+            int available = item["available_copies"].get<int>();
+            item["available_copies"] = available + 1;
+            saveJson(data);
+            return true;
         }
-        return false;
-      }
+    }
+    return false;
 
-
-      bool increaseAvailability(int bookId){
-        json data = getAllJson();
-        for (auto &item : data){
-            if(item["id"] == bookId){
-                int available = item["available_copies"].get<int>();
-                int total = item["total_copies"].get<int>();
-                if(available < total) {
-                    item["available_copies"] = available + 1;
-                    saveJson(data);
-                    return true;
-                }
-            }
-        }
-        return false;
-      }
-
-
-    private :
-        string toLower(string s ){
-            std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-            return s;
-        }
-   
-};
+}
--- Biblio Brain Project/app/models/Book.h
+++ Biblio Brain Project/app/models/Book.h
@@ -1,1 +1,30 @@
 
+#pragma once
+#include "../../core/BaseModel.hpp"
+#include <string>
+using namespace std;
+struct Book {
+    int id;
+   string title;
+    string author;
+    string category;
+    string description;
+    string imagepath;
+    int year;
+    int availableCopies;
+
+
+    Book();
+    static Book fromJson(const json& j);
+    json toJson() const;
+    bool isValid() const;
+    bool isAvailable() const;
+};
+
+class BookModel : public BaseModel<Book> {
+public:
+    BookModel();
+    void create(const Book& book);
+    bool decreaseAvailability(int bookId);
+    bool increaseAvailability(int bookId);
+};
