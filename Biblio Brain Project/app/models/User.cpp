50f3cdc486a389e6fb487025bf6de6e421a99e7d
Toka Osama <tokaosama896@gmail.com>
2025-12-10 02:17:48 +0200

    edit user model

--- Biblio Brain Project/app/models/User.cpp
+++ Biblio Brain Project/app/models/User.cpp
@@ -1,93 +1,78 @@
-#pragma once
-#include "../../core/BaseModel.hpp"
-#include <string>
-#include <algorithm>
-struct User {
-    int id;
-    string name;
-    string email;
-    string password;
-    string role;          
-    string phoneNumber;
-    string dateOfBirth;
+#include "User.h"
+using namespace std;
+User::User()
+    : id(-1), name(""), email(""), password(""),
+      role("client"), phoneNumber(""), dateOfBirth("") {}
+User User::fromJson(const json &j) {
+    User u;
+    if (j.contains("id")) u.id = j["id"].get<int>();
+    if (j.contains("name")) u.name = j["name"].get<string>();
+    if (j.contains("email")) u.email = j["email"].get<string>();
+    if (j.contains("password")) u.password = j["password"].get<string>();
+    if (j.contains("role")) u.role = j["role"].get<string>();
+    if (j.contains("phone_number")) u.phoneNumber = j["phone_number"].get<string>();
+    if (j.contains("date_of_birth")) u.dateOfBirth = j["date_of_birth"].get<string>();
+    return u;
+}
+json User::toJson() const {
+    return {
+        {"id", id},
+        {"name", name},
+        {"email", email},
+        {"password", password},
+        {"role", role},
+        {"phone_number", phoneNumber},
+        {"date_of_birth", dateOfBirth}
+    };
+}
 
-    User() : id(-1), name(""), email(""), password(""), role("client"), phoneNumber(""), dateOfBirth("") {}
- 
-    static User fromJson(const json &j) {
-        User u;
-        if(j.contains("id")) u.id = j["id"].get<int>();
-        if(j.contains("name")) u.name = j["name"].get<string>();
-        if(j.contains("email")) u.email = j["email"].get<string>();
-        if(j.contains("password")) u.password = j["password"].get<string>();
-        if(j.contains("role")) u.role = j["role"].get<string>();
-        if(j.contains("phone_number")) u.phoneNumber = j["phone_number"].get<string>();
-        if(j.contains("date_of_birth")) u.dateOfBirth = j["date_of_birth"].get<string>();
-        return u;
-    }
+bool User::isValid() const {
+    return id != -1 &&
+        !email.empty() &&
+        !name.empty() &&
+        !password.empty();
+}
+UserModel::UserModel() : BaseModel("storage/users.json") {}
 
-    json toJson() const {
-        return {
-            {"id", id},
-            {"name", name},
-            {"email", email},
-            {"password", password},
-            {"role", role},
-            {"phone_number", phoneNumber},
-            {"date_of_birth", dateOfBirth}
-        };
-    }
+void UserModel::create(const User &user) {
+    json data = getAllJson();
+    data.push_back(user.toJson());
+    data.back()["id"] = generateId();
+    saveJson(data);
+}
 
+vector<User> UserModel::search(const string &query) {
+    vector<User> results;
+    string lowerQuery = toLower(query);
 
-    bool isValid() const { return id != -1 && !email.empty(); }
-};
-
-class UserModel : public BaseModel<User> {
-  public : 
-    UserModel() : BaseModel("storage/users.json") {}
-
- 
-    void create(const User &user) {
-        json data = getAllJson();
-        data.push_back(user.toJson());
-        data.back()["id"] = generateId(); 
-        saveJson(data);
-    }
-
-    vector<User> search(const string &query) {
-        vector<User> results;
-        string lowerQuery = toLower(query);
-
-        for(auto &item : all()) {
-            if(toLower(item.name).find(lowerQuery) != string::npos || 
-               toLower(item.email).find(lowerQuery) != string::npos ||
-               toLower(item.phoneNumber).find(lowerQuery) != string::npos) {
-                
-                results.push_back(item);
-            }
+    for (auto &item : all()) {
+        if (toLower(item.name).find(lowerQuery) != string::npos ||
+            toLower(item.email).find(lowerQuery) != string::npos ||
+            toLower(item.phoneNumber).find(lowerQuery) != string::npos)
+        {
+            results.push_back(item);
         }
-        return results;
     }
+    return results;
+}
 
-    int login(const string &email, const string &password) {
-        for(auto &user : all()) {
-            if(user.email == email && user.password == password) {
-                return user.id;
-            }
+int UserModel::login(const string &email, const string &password) {
+    for (auto &user : all()) {
+        if (user.email == email && user.password == password) {
+            return user.id;
         }
-        return -1; 
     }
+    return -1;
+}
 
-    User getUserById(int id) {
-        for(auto &user : all()) {
-            if(user.id == id) return user;
-        }
-        return User(); 
+User UserModel::getUserById(int id) {
+    for (auto &user : all()) {
+        if (user.id == id) return user;
     }
+    return User();
+}
 
-private:
-    
-    string toLower(string s) {
-        transform(s.begin(), s.end(), s.begin(), ::tolower);
-        return s;
-    }
-};
+string UserModel::toLower(string s) {
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    return s;
+}
