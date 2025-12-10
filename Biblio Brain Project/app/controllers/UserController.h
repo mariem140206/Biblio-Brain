c9808b839a7c2f23e6840eb847f2377cce335514
omnia <omnya.ehabasaad@gmail.com>
2025-12-10 23:13:36 +0200

    Add UserController files

---
+++ Biblio Brain Project/app/controllers/UserController.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include "User.h"
+#include "Response.h"
+#include "AuthService.h"
+#include "crow.h"
+
+using namespace std;
+
+class UserController {
+public:
+    crow::response index(const crow::request& req);
+    crow::response updateRole(const crow::request& req, int id);
+    crow::response destroy(const crow::request& req, int id);
+};
